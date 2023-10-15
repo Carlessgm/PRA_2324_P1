5cec5e525d2079d33a7d34b559dd24ba439fb0f8
CGRAMON <cgramon@alumno.upv.es@linux-vdi02.upvnet.upv.es>
2023-10-15 21:11:38 +0200

    Añadido código de test de la clase ListLinked; Makefile actualizado; ListLinked.h modificado

--- ListLinked.h
+++ ListLinked.h
@@ -43,7 +43,7 @@ class ListLinked : public List<T> {
 		friend ostream& operator<<(ostream &out, const ListLinked<T> &list){
 			Node<T>* aux = list.first;
 
-			out << "[";
+			out << "List => [";
 			while(aux != nullptr){
 				out << aux -> data;
 				if(aux -> next != nullptr){
@@ -54,6 +54,116 @@ class ListLinked : public List<T> {
 			out << "]";
 
 			return out;
+		}
+
+		void insert(int pos, T e) override{
+			if(pos < 0 || pos > n){
+				throw out_of_range("Posición fuera de rango");
+			}
+
+			if(pos == 0){
+				Node<T>* nuevoNodo = new Node<T>(e, first);
+				first = nuevoNodo;
+			}
+			else{
+				Node<T>* aux = first;
+				for(int i = 0; i < pos - 1; i++){
+					aux = aux -> next;
+				}
+				Node<T>* nuevoNodo = new Node<T>(e, aux -> next);
+				aux -> next = nuevoNodo;
+			}
+			n++;
+		}
+
+		void append(T e) override{
+			if(first == nullptr){
+				Node<T>* nuevoNodo = new Node<T>(e);
+				first = nuevoNodo;
+			}
+			else{
+				Node<T>* aux = first;
+				while(aux -> next != nullptr){
+					aux = aux -> next;
+				}
+				Node<T>* nuevoNodo = new Node<T>(e);
+				aux -> next = nuevoNodo;
+			}
+			n++;
+		}
+
+		void prepend(T e) override{
+			Node<T>* nuevoNodo = new Node<T>(e, first);
+			first = nuevoNodo;
+			n++;
+		}
+
+		T remove(int pos) override{
+			if(pos < 0 || pos >= n){
+				throw out_of_range("Posición fuera de rango");
+			}
+
+			T ValElim;
+			
+			if(pos == 0){
+				Node<T>* aux = first;
+				ValElim = aux -> data;
+				first = first -> next;
+				delete aux;
+			}
+			else{
+				Node<T>* aux = first;
+				for(int i = 0; i < pos - 1; i++){
+					aux = aux -> next;
+				}
+				Node<T>* aux2 = aux -> next;
+				ValElim = aux2 -> data;
+				aux -> next = aux2 -> next;
+				delete aux2;
+			}
+			n--;
+			return ValElim;
+		}
+
+		T get(int pos) override{
+			if(pos < 0 || pos >= n){
+				throw out_of_range("Posición fuera de rango");
+			}
+
+			Node<T>* aux = first;
+			for(int i = 0; i < pos; i++){
+				aux = aux -> next;
+			}
+
+			return aux -> data;
+		}
+
+		int search(T e) override{
+			Node<T>* aux = first;
+			int pos = 0;
+
+			while(aux != nullptr){
+				if(aux -> data == e){
+					return pos;
+				}
+				aux = aux -> next;
+				pos++;
+			}
+			
+			return -1;
+		}
+
+		bool empty() override{
+			if(n == 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+		int size() override{
+			return n;
 		}
 
 };
