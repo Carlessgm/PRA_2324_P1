6704019109c06a5c01c9057b3948482458753ea0
CGRAMON <cgramon@alumno.upv.es@linux-vdi02.upvnet.upv.es>
2023-10-15 19:42:48 +0200

    Añadida implementación de la clase Node

---
+++ Node.h
@@ -0,0 +1,22 @@
+#include <ostream>
+
+using namespace std;
+
+template <typename T>
+class Node {
+	public:
+		T data;
+		Node<T>* next;
+
+		Node(T data, Node<T>* next = nullptr){	//"= nullptr" -> Si no se
+			this -> data = data;		//proporciona "next" es nullptr
+			this -> next = next;		//Si sí, es igual a "next"
+		}
+
+		friend ostream& operator<<(ostream &out, const Node<T> &node){
+			out << node.data;
+
+			return out;
+		}
+
+};
